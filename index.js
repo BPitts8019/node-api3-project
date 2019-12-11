85b1d34854eff96efe500284ff5b74de090a7c7c
BPitts8019 <bradleyj.pitts@gmail.com>
2019-12-10 18:00:53 -0700

    Added Express boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code away!
+const express = require("express");
+const server = express();
+const usersRouter = require("./routers/users");
+
+//apply global middleware
+server.use(express.json());
+
+//enpoint routing
+server.get("/", (req, res) => {
+   res.send("<h2>Welcome to SUM SOCIAL API</h>");
+});
+
+server.use("/api/users", usersRouter);
+
+//start the server
+const port = 8080;
+server.listen(port, () => {
+   console.log(`Server running at http://localhost:${port}`);
+});
