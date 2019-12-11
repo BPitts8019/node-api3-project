20a8e41d7d20d8b054706441ad48f0fdca7dff2d
BPitts8019 <bradleyj.pitts@gmail.com>
2019-12-10 20:00:19 -0700

    Adding preformed files into project

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-const express = require("express");
-const server = express();
-const userRouter = require("./users/userRouter");
-
-//apply global middleware
-server.use(express.json());
-
-//enpoint routing
-/**
- * GET	/api
- * Returns an array of all the API enpoints and posible methods for use. 
- * @returns {Array} API endpoints and methods
- */
-server.get("/api", (req, res) => {
-   res.json(["nothing here... yet"]);
-});
-
-server.use("/api/users", userRouter);
-
-//404 not found
-server.use((req, res) => {
-   res.status(404).json({
-      message: "Route was not found"
-   });
-});
+const server = require("./server");
+const port = 8080;
 
 //start the server
-const port = 8080;
 server.listen(port, () => {
    console.log(`Server running at http://localhost:${port}`);
 });
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
-
+const userRouter = require("./users/userRouter");
 const server = express();
 
+//apply global middleware
+server.use(express.json());
+server.use(logger);
+
+//enpoint routing
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use("/api/users", userRouter);
+
+//404 not found
+server.use((req, res) => {
+   res.status(404).json({
+      message: "Route was not found"
+   });
 });
 
 //custom middleware
-
 function logger(req, res, next) {}
 
 module.exports = server;
