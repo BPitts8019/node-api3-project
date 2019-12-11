967a828dc5a098c152b3eb41c06ca482901b40c0
BPitts8019 <bradleyj.pitts@gmail.com>
2019-12-10 18:44:30 -0700

    Discovered routers were already made

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const server = express();
-const welcomeRouter = require("./routers/welcome");
 const usersRouter = require("./routers/users");
+const postsRouter = require("./routers/posts");
 
 //apply global middleware
 server.use(express.json());
 
 //enpoint routing
-server.use("/", welcomeRouter);
+/**
+ * GET	/api
+ * Returns an array of all the API enpoints and posible methods for use. 
+ * @returns {Array} API endpoints and methods
+ */
+server.get("/api", (req, res) => {
+   res.json(["nothing here... yet"]);
+});
+
 server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
+
+//404 not found
+server.use((req, res) => {
+   res.status(404).json({
+      message: "Route was not found"
+   });
+});
 
 //start the server
 const port = 8080;
